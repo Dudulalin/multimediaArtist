4650aa305210a7f427554ac07258f1c41f73909e
Sitmonternna <2163597@cmontmorency.qc.ca>
2025-02-08 17:45:51 -0500

    about-header-static-ordi

--- main.js
+++ main.js
@@ -20,3 +20,12 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+ // Toggle the menu
+ const burgerMenu = document.querySelector('.burger-menu');
+ const navMenu = document.querySelector('.nav-menu');
+
+ burgerMenu.addEventListener('click', () => {
+     burgerMenu.classList.toggle('open');
+     navMenu.classList.toggle('open');
+ });
